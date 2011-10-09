f69b94ce1fc0fc1a47e340ba6b5ac05418fdac64
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2011-10-10 05:23:56 +0900

    updated sweeper

--- app/models/circulation_sweeper.rb
+++ app/models/circulation_sweeper.rb
@@ -1,5 +1,5 @@
 class CirculationSweeper < ActionController::Caching::Sweeper
-  observe Basket, Checkin
+  observe Basket, Checkin, Checkout
   include ExpireEditableFragment
 
   def after_save(record)
@@ -10,6 +10,8 @@ class CirculationSweeper < ActionController::Caching::Sweeper
         expire_editable_fragment(checkout.item.manifestation, ['holding', 'show_list'], ['html', 'mobile'])
       end
     when record.is_a?(Checkin)
+      expire_editable_fragment(record.item.manifestation, ['holding', 'show_list'], ['html', 'mobile'])
+    when record.is_a?(Checkout)
       expire_editable_fragment(record.item.manifestation, ['holding', 'show_list'], ['html', 'mobile'])
     end
   end
