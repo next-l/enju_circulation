3d714414c7f51d4be347a2c663209509467660e1
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2012-03-18 21:40:47 +0900

    updated sweeper

--- app/models/circulation_sweeper.rb
+++ app/models/circulation_sweeper.rb
@@ -6,13 +6,13 @@ class CirculationSweeper < ActionController::Caching::Sweeper
     case
     when record.is_a?(Basket)
       record.checkouts.each do |checkout|
-        expire_editable_fragment(checkout.item, ['detail'])
-        expire_editable_fragment(checkout.item.manifestation, ['holding', 'show_list'], ['html', 'mobile'])
+        expire_editable_fragment(checkout.item)
+        expire_editable_fragment(checkout.item.manifestation)
       end
     when record.is_a?(Checkin)
-      expire_editable_fragment(record.item.manifestation, ['holding', 'show_list'], ['html', 'mobile'])
+      expire_editable_fragment(record.item.manifestation)
     when record.is_a?(Checkout)
-      expire_editable_fragment(record.item.manifestation, ['holding', 'show_list'], ['html', 'mobile'])
+      expire_editable_fragment(record.item.manifestation)
     end
   end
 
