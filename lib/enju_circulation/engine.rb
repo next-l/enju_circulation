d279850ca7de6bd96bac9f82b596d02a9b5771a4
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2012-06-19 10:44:22 +0900

    require enju_core

--- lib/enju_circulation/engine.rb
+++ lib/enju_circulation/engine.rb
@@ -1,15 +1,9 @@
-require "devise"
-require "cancan"
+require "enju_core"
 require "validates_timeliness"
-require "attribute_normalizer"
 require "inherited_resources"
 require "state_machine"
-require "friendly_id"
-require "will_paginate"
-require "sunspot_rails"
 require "enju_message"
 require "enju_event"
-require "acts_as_list"
 
 module EnjuCirculation
   class Engine < ::Rails::Engine
