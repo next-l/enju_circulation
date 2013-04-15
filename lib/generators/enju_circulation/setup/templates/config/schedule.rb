4c20699c3cd7ef23b83af7936c8fba799fb53f3f
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2013-04-15 14:18:58 +0900

    fixed generator

--- lib/generators/enju_circulation/setup/templates/config/schedule.rb
+++ lib/generators/enju_circulation/setup/templates/config/schedule.rb
@@ -10,10 +10,8 @@ end
 
 every 1.day, :at => '1:00 am' do
   rake "enju:circulation:stat"
-#  rake "enju:bookmark:stat"
 end
 
 every 1.day, :at => '5:00 am' do
   rake "enju:circulation:send_notification"
-  rake "enju:import:destroy"
 end
