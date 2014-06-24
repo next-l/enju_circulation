76893f8adfc72eb680b491248a8598ffb1b89de9
Kosuke Tanabe <nabeta@fastmail.fm>
2014-06-24 15:16:06 +0900

    add "started" state

--- app/models/manifestation_checkout_stat_state_machine.rb
+++ app/models/manifestation_checkout_stat_state_machine.rb
@@ -1,11 +1,12 @@
 class ManifestationCheckoutStatStateMachine
   include Statesman::Machine
   state :pending, initial: true
+  state :started
   state :completed
 
-  transition from: :pending, to: :completed
+  transition from: :pending, to: [:started, :completed]
 
   before_transition(to: :completed) do |manifestation_checkout_stat|
-    manifestation_checkout_stat.calculate_count
+    manifestation_checkout_stat.calculate_count!
   end
 end
