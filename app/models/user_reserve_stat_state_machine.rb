40dd2074a58e2f37543d126855771c4d2bd52c52
Kosuke Tanabe <nabeta@fastmail.fm>
2014-07-02 18:39:08 +0900

    update completed_at

--- app/models/user_reserve_stat_state_machine.rb
+++ app/models/user_reserve_stat_state_machine.rb
@@ -8,6 +8,11 @@ class UserReserveStatStateMachine
   transition from: :started, to: :completed
 
   after_transition(to: :started) do |user_reserve_stat|
+    user_reserve_stat.update_column(:started_at, Time.zone.now)
     user_reserve_stat.calculate_count!
+  end
+
+  after_transition(to: :started) do |user_reserve_stat|
+    user_reserve_stat.update_column(:completed_at, Time.zone.now)
   end
 end
