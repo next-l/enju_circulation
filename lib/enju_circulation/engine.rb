02fb9bc68e3fc6706cddb620e73426f09d512697
Kosuke Tanabe <nabeta@fastmail.fm>
2014-08-14 23:59:30 +0900

    cleaned up

--- lib/enju_circulation/engine.rb
+++ lib/enju_circulation/engine.rb
@@ -1,10 +1,7 @@
 require "enju_biblio"
 require "enju_library"
-require "validates_timeliness"
-require "inherited_resources"
 require "enju_message"
 require "enju_event"
-require "statesman"
 
 module EnjuCirculation
   class Engine < ::Rails::Engine
