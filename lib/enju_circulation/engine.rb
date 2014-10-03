66df01532f21b9570c4a114b8503962070549329
Kosuke Tanabe <nabeta@fastmail.fm>
2014-10-03 18:39:22 +0900

    require protected_attributes in gemfile

--- lib/enju_circulation/engine.rb
+++ lib/enju_circulation/engine.rb
@@ -2,7 +2,6 @@ require "enju_biblio"
 require "enju_library"
 require "enju_message"
 require "enju_event"
-require "protected_attributes" if Rails::VERSION::MAJOR == 4
 
 module EnjuCirculation
   class Engine < ::Rails::Engine
