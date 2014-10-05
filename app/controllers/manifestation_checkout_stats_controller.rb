037ef9d13d9712e32c5ed664264eb38dd11beb02
Kosuke Tanabe <nabeta@fastmail.fm>
2014-10-05 22:52:23 +0900

    removed tag

--- app/controllers/manifestation_checkout_stats_controller.rb
+++ app/controllers/manifestation_checkout_stats_controller.rb
@@ -34,7 +34,7 @@ class ManifestationCheckoutStatsController < ApplicationController
       Checkout.arel_table[:created_at].lt @manifestation_checkout_stat.end_date
     ).joins(item: [:shelf, :manifestation]).group(:carrier_type_id).merge(
       Manifestation.where(carrier_type_id: CarrierType.pluck(:id))
-    ).order(:carrier_type_id).count(:id) %>
+    ).order(:carrier_type_id).count(:id)
 
     respond_to do |format|
       format.html # show.html.erb
