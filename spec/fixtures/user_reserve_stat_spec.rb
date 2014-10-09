050e21920edf7827d012cf97fc6b9903eea0030d
Kosuke Tanabe <nabeta@fastmail.fm>
2014-10-10 01:08:07 +0900

    added fixture files

---
+++ spec/fixtures/user_reserve_stat_spec.rb
@@ -0,0 +1,32 @@
+# -*- encoding: utf-8 -*-
+require 'spec_helper'
+
+describe UserReserveStat do
+  fixtures :user_reserve_stats
+
+  it "calculates user count" do
+    old_message_count = Message.count
+    user_reserve_stats(:one).transition_to!(:started).should be_truthy
+    Message.count.should eq old_message_count + 1
+    Message.order(:id).last.subject.should eq '集計が完了しました'
+  end
+
+  it "should calculate in background" do
+    UserReserveStatQueue.perform(user_reserve_stats(:one).id).should be_truthy
+  end
+end
+
+# == Schema Information
+#
+# Table name: user_reserve_stats
+#
+#  id           :integer          not null, primary key
+#  start_date   :datetime
+#  end_date     :datetime
+#  note         :text
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  started_at   :datetime
+#  completed_at :datetime
+#  user_id      :integer
+#
