d318ef8c6a9793ddc205f2e733b32a4e723baca5
Kosuke Tanabe <nabeta@fastmail.fm>
2015-01-13 21:38:29 +0900

    set default time to the beginning of day next-l/enju_leaf#502

--- app/controllers/manifestation_checkout_stats_controller.rb
+++ app/controllers/manifestation_checkout_stats_controller.rb
@@ -60,6 +60,8 @@ class ManifestationCheckoutStatsController < ApplicationController
   # GET /manifestation_checkout_stats/new.json
   def new
     @manifestation_checkout_stat = ManifestationCheckoutStat.new
+    @manifestation_checkout_stat.start_date = Time.zone.now.beginning_of_day
+    @manifestation_checkout_stat.end_date = Time.zone.tomorrow.beginning_of_day
 
     respond_to do |format|
       format.html # new.html.erb
