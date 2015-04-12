2b6bee9f5ba4878258bff1c1b0b2e6a4ef1ed44e
Kosuke Tanabe <nabeta@fastmail.fm>
2015-04-12 22:17:48 +0900

    updated spec files

--- spec/views/checkouts/edit.html.erb_spec.rb
+++ spec/views/checkouts/edit.html.erb_spec.rb
@@ -14,8 +14,6 @@ describe "checkouts/edit" do
 
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "form", action: checkouts_path(@checkout), method: "post" do
-      assert_select "input#checkout_user_id", name: "checkout[user_id]"
-      assert_select "input#checkout_item_id", name: "checkout[item_id]"
       assert_select "input#checkout_due_date", name: "checkout[due_date]"
     end
   end
