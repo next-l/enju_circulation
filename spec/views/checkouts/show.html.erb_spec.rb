b1ba0958c6980e0d87ff953ed722a505076e8971
Kosuke Tanabe <nabeta@fastmail.fm>
2015-09-14 02:30:51 +0900

    update spec file

--- spec/views/checkouts/show.html.erb_spec.rb
+++ spec/views/checkouts/show.html.erb_spec.rb
@@ -14,8 +14,8 @@ describe "checkouts/show" do
   it "renders attributes in <p>" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    rendered.should match(/返却期限/)
+    rendered.should match(/Due date/)
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    rendered.should match(/所蔵情報ID/)
+    rendered.should match(/Item identifier/)
   end
 end
