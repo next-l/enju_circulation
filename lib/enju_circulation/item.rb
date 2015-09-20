62a16284485a9f873b9612714c3acc617790d5ee
Masao Takaku <tmasao@acm.org>
2015-09-20 09:56:19 +0900

    add condition for #for_checkout. cf. next-l/enju_leaf#735

--- lib/enju_circulation/item.rb
+++ lib/enju_circulation/item.rb
@@ -27,10 +27,12 @@ module EnjuCirculation
         include InstanceMethods
         has_many :reserves, :foreign_key => :manifestation_id
 
-        scope :for_checkout, -> { includes(:circulation_status, :use_restriction).where(
+        scope :for_checkout, ->(identifier_conditions = 'item_identifier IS NOT NULL') {
+          includes(:circulation_status, :use_restriction).where(
             'circulation_statuses.name' => FOR_CHECKOUT_CIRCULATION_STATUS,
             'use_restrictions.name' => FOR_CHECKOUT_USE_RESTRICTION
-          ).where('item_identifier IS NOT NULL') }
+          ).where(identifier_conditions)
+	}
         scope :removed, -> { includes(:circulation_status).where('circulation_statuses.name' => 'Removed') }
         has_many :checkouts
         has_many :reserves
