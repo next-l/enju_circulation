0bd174821ea10d731d8858c7e0440a8ed8d0cb77
Kosuke Tanabe <nabeta@fastmail.fm>
2015-10-14 01:51:55 +0900

    revert updating circulation_status next-l/enju_leaf#860

--- lib/enju_circulation/withdraw.rb
+++ lib/enju_circulation/withdraw.rb
@@ -14,7 +14,7 @@ module EnjuCirculation
     module InstanceMethods
       def withdraw!
         circulation_status = CirculationStatus.where(name: 'Removed').first
-        item.circulation_status = circulation_status
+        item.update_column(:circulation_status_id, circulation_status.id) if circulation_status
         item.use_restriction = UseRestriction.where(name: 'Not For Loan').first
         item.index!
       end
