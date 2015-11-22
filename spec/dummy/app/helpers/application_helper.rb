52b92510d3177bb4464c30130828a4ee4bddd394
Kosuke Tanabe <nabeta@fastmail.fm>
2015-11-23 03:53:12 +0900

    fix dummy file

--- spec/dummy/app/helpers/application_helper.rb
+++ spec/dummy/app/helpers/application_helper.rb
@@ -1,7 +1,3 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-  include EnjuLeaf::EnjuLeafHelper
-  include EnjuBiblio::BiblioHelper if defined?(EnjuBiblio)
-  include PictureFilesHelper if defined?(EnjuLibrary)
-  include EnjuManifestationViewer::ManifestationViewerHelper if defined?(EnjuManifestationViewer)
 end
