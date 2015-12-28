cb06222e49d6ab044de206ba30c23ca74d4a6b45
Masao Takaku <tmasao@acm.org>
2015-12-28 22:18:19 +0900

    fix install script. cf. next-l/enju_leaf#1061

--- lib/generators/enju_circulation/setup/setup_generator.rb
+++ lib/generators/enju_circulation/setup/setup_generator.rb
@@ -14,6 +14,16 @@ class EnjuCirculation::SetupGenerator < Rails::Generators::Base
       "  enju_circulation\n", :after => "enju_library\n"
     inject_into_file 'app/models/user.rb',
       "  enju_circulation_user_model\n", :after => "enju_leaf_user_model\n"
+    append_to_file "config/initilizers/enju_leaf.rb", <<EOS
+Accept.include(EnjuCirculation::EnjuAccept)
+Basket.include(EnjuCirculation::EnjuBasket)
+CarrierType.include(EnjuCirculation::EnjuCarrierType)
+Manifestation.include(EnjuCirculation::EnjuManifestation)
+Item.include(EnjuCirculation::EnjuItem)
+Profile.include(EnjuCirculation::EnjuProfile)
+UserGroup.include(EnjuCirculation::EnjuUserGroup)
+Withdraw.include(EnjuCirculation::EnjuWithdraw)
+EOS
     append_to_file("config/schedule.rb", File.open(File.expand_path('../templates', __FILE__) + '/config/schedule.rb').read)
     append_to_file 'config/initializers/inflections.rb',  <<EOS
 ActiveSupport::Inflector.inflections do |inflect|
