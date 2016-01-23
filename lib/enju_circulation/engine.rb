b4898bfc50f644cdc8db69ea6d67a4c709d72d81
Kosuke Tanabe <nabeta@fastmail.fm>
2016-01-23 14:56:49 -0500

    update generator

--- lib/enju_circulation/engine.rb
+++ lib/enju_circulation/engine.rb
@@ -1,8 +1,5 @@
-require "enju_biblio"
-require "enju_library"
-require "enju_message"
+require "enju_leaf"
 require "enju_event"
-require "cocoon"
 
 module EnjuCirculation
   class Engine < ::Rails::Engine
--- lib/generators/enju_circulation/setup/setup_generator.rb
+++ lib/generators/enju_circulation/setup/setup_generator.rb
@@ -10,10 +10,22 @@ class EnjuCirculation::SetupGenerator < Rails::Generators::Base
     rake("enju_message_engine:install:migrations")
     generate("enju_event:setup")
     generate("enju_message:setup")
-    inject_into_file 'app/controllers/application_controller.rb',
-      "  enju_circulation\n", :after => "enju_library\n"
-    inject_into_file 'app/models/user.rb',
-      "  enju_circulation_user_model\n", :after => "enju_leaf_user_model\n"
+    inject_into_class "app/controllers/application_controller.rb", ApplicationController do
+      "  include EnjuCirculation::Controller\n"
+    end
+    inject_into_class 'app/models/user.rb', User do
+      "  include EnjuCirculation::EnjuUser\n"
+    end
+    append_to_file "config/initializers/enju_leaf.rb", <<EOS
+Accept.include(EnjuCirculation::EnjuAccept)
+Basket.include(EnjuCirculation::EnjuBasket)
+CarrierType.include(EnjuCirculation::EnjuCarrierType)
+Manifestation.include(EnjuCirculation::EnjuManifestation)
+Item.include(EnjuCirculation::EnjuItem)
+Profile.include(EnjuCirculation::EnjuProfile)
+UserGroup.include(EnjuCirculation::EnjuUserGroup)
+Withdraw.include(EnjuCirculation::EnjuWithdraw)
+EOS
     append_to_file("config/schedule.rb", File.open(File.expand_path('../templates', __FILE__) + '/config/schedule.rb').read)
     append_to_file 'config/initializers/inflections.rb',  <<EOS
 ActiveSupport::Inflector.inflections do |inflect|
