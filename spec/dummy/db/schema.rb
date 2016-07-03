6697dc34b03659ce2ce0f93a37a092a6f9f8ac0d
Kosuke Tanabe <nabeta@fastmail.fm>
2016-07-04 04:03:07 +0900

    add foreign key on manifestation_id

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160703184805) do
+ActiveRecord::Schema.define(version: 20160703190209) do
 
   create_table "accepts", force: :cascade do |t|
     t.integer  "basket_id"
