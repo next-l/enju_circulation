da5ced0e420ee7b25e2e192e79832a2036b25f5b
Kosuke Tanabe <nabeta@fastmail.fm>
2016-08-12 16:35:08 +0900

    update dummy files

--- spec/dummy/app/controllers/application_controller.rb
+++ spec/dummy/app/controllers/application_controller.rb
@@ -1,9 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  include EnjuLeaf::Controller
-  include EnjuBiblio::Controller
   include EnjuLibrary::Controller
+  include EnjuBiblio::Controller
   include EnjuCirculation::Controller
   before_action :set_paper_trail_whodunnit
   after_action :verify_authorized
