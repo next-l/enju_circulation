a8d1008cfb9d60cba3c825dab8e4000a515610ce
Kosuke Tanabe <nabeta@fastmail.fm>
2016-08-20 14:04:50 +0900

    fix generator

--- lib/generators/enju_circulation/setup/setup_generator.rb
+++ lib/generators/enju_circulation/setup/setup_generator.rb
@@ -5,11 +5,11 @@ class EnjuCirculation::SetupGenerator < Rails::Generators::Base
   def copy_setup_files
     directory("db/fixtures", "db/fixtures/enju_circulation")
     return if file == 'fixture'
-    rake("enju_circulation_engine:install:migrations")
-    rake("enju_event_engine:install:migrations")
     rake("enju_message_engine:install:migrations")
-    generate("enju_event:setup")
+    rake("enju_event_engine:install:migrations")
+    rake("enju_circulation_engine:install:migrations")
     generate("enju_message:setup")
+    generate("enju_event:setup")
     inject_into_class "app/controllers/application_controller.rb", ApplicationController do
       "  include EnjuCirculation::Controller\n"
     end
