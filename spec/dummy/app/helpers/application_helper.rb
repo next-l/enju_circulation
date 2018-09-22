f6c37b9142801c00eb0c7f6b3073b95021be3cb0
Kosuke Tanabe <nabeta@fastmail.fm>
2018-09-22 14:09:09 +0900

    update dummy helper file

--- spec/dummy/app/helpers/application_helper.rb
+++ spec/dummy/app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-  include EnjuLeaf::EnjuLeafHelper
-  include EnjuBiblio::BiblioHelper if defined?(EnjuBiblio)
+  include EnjuLeaf::AppicationHelper
+  include EnjuBiblio::ApplicationHelper
 end
