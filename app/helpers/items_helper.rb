4689aacd0da169ca95663889352dbdf35d7e04a1
Kosuke Tanabe <nabeta@fastmail.fm>
2019-01-18 04:03:06 +0900

    remove display_name.localize

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -7,10 +7,10 @@ module ItemsHelper
       current = true if params[:circulation_status] == circulation_status.name
       if current
         content_tag :strong do
-          link_to("#{circulation_status.display_name.localize} (" + facet.count.to_s + ")", url_for(filtered_params.merge(circulation_status: circulation_status.name, page: nil, view: nil, only_path: true)))
+          link_to("#{circulation_status.display_name} (" + facet.count.to_s + ")", url_for(filtered_params.merge(circulation_status: circulation_status.name, page: nil, view: nil, only_path: true)))
         end
       else
-        link_to("#{circulation_status.display_name.localize} (" + facet.count.to_s + ")", url_for(filtered_params.merge(circulation_status: circulation_status.name, page: nil, view: nil, only_path: true)))
+        link_to("#{circulation_status.display_name} (" + facet.count.to_s + ")", url_for(filtered_params.merge(circulation_status: circulation_status.name, page: nil, view: nil, only_path: true)))
       end
     end
   end
