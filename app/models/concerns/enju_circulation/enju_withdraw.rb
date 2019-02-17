808cd42651270dbd3217ebe124e06e4630e173d2
Kosuke Tanabe <nabeta@fastmail.fm>
2019-02-18 02:22:11 +0900

    fix setting circulation_status

--- app/models/concerns/enju_circulation/enju_withdraw.rb
+++ app/models/concerns/enju_circulation/enju_withdraw.rb
@@ -7,7 +7,7 @@ module EnjuCirculation
     end
 
     def withdraw!
-      item.update!(circulation_status: CirculationStatus.find_by(name: 'Removed'))
+      item.update_column(:circulation_status_id, CirculationStatus.find_by(name: 'Removed').id)
       item.use_restriction = UseRestriction.find_by(name: 'Not For Loan')
       item.index!
     end
