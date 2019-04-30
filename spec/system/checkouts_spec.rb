9c75471a99be5c60a8153ec0caabf3c1e1066629
Kosuke Tanabe <nabeta@fastmail.fm>
2019-05-01 04:44:08 +0900

    add system spec next-l/enju_leaf#990

---
+++ spec/system/checkouts_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe 'Checkouts', type: :system do
+  include Devise::Test::IntegrationHelpers
+  fixtures :all
+
+  describe 'When not logged in', solr: true do
+    before(:each) do
+      Checkout.reindex
+    end
+
+    it 'should contain query params in the facet' do
+      sign_in users(:librarian1)
+      visit checkouts_path(days_overdue: 10)
+      expect(page).to have_link 'RSS', href: checkouts_path(format: :rss, days_overdue: 10)
+      expect(page).to have_link 'TSV', href: checkouts_path(format: :txt, days_overdue: 10, locale: 'ja')
+    end
+  end
+end
