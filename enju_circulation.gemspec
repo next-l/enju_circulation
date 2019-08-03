6fcd15bb6c93ae08c902b5e6ef4a332406b5300b
Kosuke Tanabe <nabeta@fastmail.fm>
2019-08-03 18:56:12 +0900

    bump version

--- enju_circulation.gemspec
+++ enju_circulation.gemspec
@@ -26,7 +26,6 @@ Gem::Specification.new do |s|
   s.add_development_dependency "factory_bot_rails"
   s.add_development_dependency "mysql2"
   s.add_development_dependency "pg"
-  s.add_development_dependency "redis-rails"
   s.add_development_dependency "resque"
   s.add_development_dependency "rspec-activemodel-mocks"
   s.add_development_dependency "rspec-rails", "~> 3.8"
--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -1353,7 +1353,8 @@ ActiveRecord::Schema.define(version: 2018_01_07_162048) do
     t.integer "role_id", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.index ["user_id", "role_id"], name: "index_user_has_roles_on_user_id_and_role_id", unique: true
+    t.index ["role_id"], name: "index_user_has_roles_on_role_id"
+    t.index ["user_id"], name: "index_user_has_roles_on_user_id"
   end
 
   create_table "user_import_file_transitions", force: :cascade do |t|
