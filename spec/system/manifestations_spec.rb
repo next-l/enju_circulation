909f515da2a7c9aba1c2c6560069130c73f17229
Kosuke Tanabe <nabeta@fastmail.fm>
2019-08-11 19:06:00 +0900

    move some tempates from enju_biblio

---
+++ spec/system/manifestations_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe 'Checkouts', type: :system do
+  include Devise::Test::IntegrationHelpers
+  fixtures :all
+
+  before(:each) do
+    @item = FactoryBot.create(:item)
+    @item.use_restriction = UseRestriction.find_by(name: 'Not For Loan')
+    @item.save
+    CarrierType.find_by(name: 'volume').update(attachment: File.open("#{Rails.root.to_s}/../dummy/app/assets/images/icons/book.png"))
+  end
+
+  describe 'When logged in as Librarian' do
+    it 'should contain use_restriction' do
+      sign_in users(:librarian1)
+      visit manifestation_path(@item.manifestation)
+      expect(page).to have_content 'Not For Loan'
+    end
+  end
+
+  describe 'When not logged in' do
+    it 'should contain use_restriction' do
+      sign_in users(:librarian1)
+      visit manifestation_path(@item.manifestation)
+      expect(page).to have_content 'Not For Loan'
+    end
+  end
+end
