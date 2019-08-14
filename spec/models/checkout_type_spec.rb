3e2d4c864900a4373d93643efbc39d7138a92d15
Kosuke Tanabe <nabeta@fastmail.fm>
2019-08-15 02:05:26 +0900

    update annotations

--- spec/models/checkout_type_spec.rb
+++ spec/models/checkout_type_spec.rb
@@ -9,11 +9,12 @@ end
 #
 # Table name: checkout_types
 #
-#  id           :integer          not null, primary key
-#  name         :string           not null
-#  display_name :text
-#  note         :text
-#  position     :integer
-#  created_at   :datetime
-#  updated_at   :datetime
+#  id                        :integer          not null, primary key
+#  name                      :string           not null
+#  display_name              :text
+#  note                      :text
+#  position                  :integer
+#  created_at                :datetime
+#  updated_at                :datetime
+#  display_name_translations :jsonb            not null
 #
