76ffdcb9e356c588fbb7b9bc66c7c268b25c701a
Kosuke Tanabe <nabeta@fastmail.fm>
2019-08-16 00:53:48 +0900

    add :lockable

--- spec/dummy/app/models/user.rb
+++ spec/dummy/app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, #:registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :lockable
 
   include EnjuSeed::EnjuUser
   include EnjuMessage::EnjuUser
