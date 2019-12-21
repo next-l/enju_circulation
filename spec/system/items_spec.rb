fc7592e4c19802861985fccccfcf1bcca3f00879
Kosuke Tanabe <nabeta@fastmail.fm>
2019-12-22 01:04:19 +0900

    add spec file next-l/enju_leaf#790 next-l/enju_leaf#791

---
+++ spec/system/items_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'Checkouts', type: :system do
+  include Devise::Test::IntegrationHelpers
+  fixtures :all
+
+  before(:each) do
+    CarrierType.find_by(name: 'volume').attachment.attach(io: File.open("#{Rails.root.to_s}/app/assets/images/icons/book.png"), filename: 'book.png')
+  end
+
+  describe 'When logged in as Librarian' do
+    it 'should not set "Removed" as circulation status if the item is reserved' do
+      item = FactoryBot.create(:reserve).item
+      sign_in users(:librarian1)
+      visit edit_item_path(item)
+      select('除籍済み', from: 'item_circulation_status_id')
+      click_button('更新する')
+      expect(page).to have_content 'この貸出状態には変更できません。資料が予約されています。'
+    end
+  end
+end
