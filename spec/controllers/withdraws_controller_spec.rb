18290b82bd5036179dbc4b8805c4df43479477c6
Kosuke Tanabe <nabeta@fastmail.fm>
2020-05-17 15:46:03 +0900

    add spec file

---
+++ spec/controllers/withdraws_controller_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe WithdrawsController do
+  fixtures :all
+  let(:valid_attributes) do
+    FactoryBot.build(:withdraw).attributes.with_indifferent_access
+  end
+  let(:valid_create_attributes) do
+    { basket_id: Basket.find(valid_attributes[:basket_id]).id,
+      withdraw: { item_identifier: Item.find(valid_attributes[:item_id]).item_identifier }}
+  end
+
+  describe 'POST create' do
+    describe 'When logged in as Administrator' do
+      login_fixture_admin
+
+      it 'should not withdraw a checked-out item' do
+        expect do
+          post :create, params: { basket_id: valid_create_attributes[:basket_id], withdraw: { item_identifier: reserves(:reserve_00014).item.item_identifier } }
+        end.to change(Withdraw, :count).by(0)
+        expect(assigns(:withdraw)).to be_a(Withdraw)
+        expect(response).to be_successful
+      end
+    end
+  end
+end
