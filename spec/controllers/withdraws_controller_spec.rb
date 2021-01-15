11156bf82a9ffa94f42e48e266989f63829451bb
Kosuke Tanabe <nabeta@fastmail.fm>
2021-01-16 05:07:07 +0900

    update spec files

--- spec/controllers/withdraws_controller_spec.rb
+++ spec/controllers/withdraws_controller_spec.rb
@@ -14,10 +14,16 @@ describe WithdrawsController do
     describe 'When logged in as Administrator' do
       login_fixture_admin
 
-      it 'should not withdraw a checked-out item' do
+      it 'should not withdraw a reserved item' do
         expect do
           post :create, params: { basket_id: valid_create_attributes[:basket_id], withdraw: { item_identifier: reserves(:reserve_00014).item.item_identifier } }
         end.to change(Withdraw, :count).by(0)
+        expect(assigns(:withdraw)).to be_a(Withdraw)
+        expect(response).to be_successful
+      end
+
+      it 'should not withdraw a checked-out item' do
+        post :create, params: { basket_id: valid_create_attributes[:basket_id], withdraw: { item_identifier: '00001' } }
         expect(assigns(:withdraw)).to be_a(Withdraw)
         expect(response).to be_successful
       end
